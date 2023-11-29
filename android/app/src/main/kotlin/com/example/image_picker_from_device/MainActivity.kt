db8b6ec16eb55426cadfbf1c795fc70fb7dea670
Md. Meraj Hossain <meraj@nerddevs.com>
2023-11-29 18:17:41 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/image_picker_from_device/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.image_picker_from_device
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker_from_device/functions/modal_bottom_sheet_menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Image Process',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File? _image;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterImage Process'),
+      ),
+      body: Center(
+        child: _image != null
+            ? Image.file(
+                _image!,
+                width: 300,
+                height: 300,
+              )
+            : const Text('No image selected.'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add_a_photo),
+        onPressed: () async => await modalBottomSheetMenu(context)
+            .then((pk) => setState(() => _image = pk)),
+      ),
+    );
+  }
+}
